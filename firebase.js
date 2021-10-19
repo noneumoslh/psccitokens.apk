09369ccdce78b892b5259ead690fa2c5462c6899
psccitoken <79065602+psccitoken12@users.noreply.github.com>
2021-10-19 16:12:37 +0700

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA4ALoI72oFLaLNnrYsSNE5lYY0Bge-tpE",
+  authDomain: "psccitoken12.firebaseapp.com",
+  projectId: "psccitoken12",
+  storageBucket: "psccitoken12.appspot.com",
+  messagingSenderId: "806616688489",
+  appId: "1:806616688489:web:8f2e5353cfdfb371103ff8",
+  measurementId: "G-WRPYW9L34N"
+};
